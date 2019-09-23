24e4a9147fd78c769cb063c6b3ce93f21453d653
jeremiah <jeremiah.kim@hotmail.com>
2019-09-22 17:02:36 -0700

    refactored and trail information added

--- server.js
+++ server.js
@@ -1,67 +1,51 @@
 'use strict';
 
-/**
- * Dependencies
- */
-
+// Dependencies
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const superagent = require('superagent');
 const pg = require('pg');
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
 const yelp = require('yelp-fusion');
+
+const app = express();
 app.use(cors());
+require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
 const client = new pg.Client(process.env.DATABASE_URL);
-console.log(process.env.DATABASE_URL);
-client.on('error', err => console.error(err));
 
-client.connect()
+client.on('error', err => console.error(err));
+client
+  .connect()
   .then(() => {
     app.listen(PORT, () => console.log(`listening on ${PORT}`));
   })
   .catch(error => handleError(error));
 
-/**
- * Routes
- */
 
-app.get('/location', routeLocation);
+// Routes
+app.get('/location', getLocation);
 app.get('/weather', getWeather);
 app.get('/events', getEvents);
 app.get('/movies', getMovies);
 app.get('/yelp', getYelp);
+app.get('/trails', getTrails);
 app.use('*', wildcardRouter);
 
-/**
- * Routers
- */
 
-function routeLocation(request, response) {
+// A function to retreive the searched location and render it on the page
+function getLocation(request, response) {
   const searchQuery = request.query.data;
-
-  try{
-    getLocation(searchQuery, response);
-  }
-  catch(err){
-    handleError(err, response);
-  }
-}
-
-function getLocation(searchQuery, response) {
-  const sql = 'SELECT * FROM locations;';
+  const sql = 'SELECT * FROM locations  WHERE search_query=$1;';
+  const value = [searchQuery];
 
   client
-    .query(sql)
-    .then(result => {
-      let match = result.rows.filter(city => (city.search_query === searchQuery));
-      match.length > 0 ? delete match[0].id && response.status(200).send(match[0]) : newLocation(searchQuery, response);})
-    .catch(err => handleError(err, response));
+    .query(sql, value)
+    .then(result => result.rows[0] ? response.status(200).send(result.rows[0]) : newLocation(searchQuery, response))
+    .catch(error => handleError(error, response));
 }
 
-function newLocation(searchQuery, response){
+function newLocation(searchQuery, response) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}`;
 
   superagent
@@ -76,38 +60,37 @@ function newLocation(searchQuery, response){
       client
         .query(sql, value)
         .then(response.status(200).send(location))
-        .catch(error => handleError(error));
+        .catch(error => handleError(error, response));
     })
+    .catch(error => handleError(error, response));
 }
 
-function Location(searchQuery, locationData){
+function Location(searchQuery, locationData) {
   this.search_query = searchQuery;
   this.formatted_query = locationData.formatted_address;
   this.latitude = locationData.geometry.location.lat;
   this.longitude = locationData.geometry.location.lng;
 }
 
+
+// A function to retreive weather information for the searched location and render it on the page
 function getWeather(request, response) {
   const searchQuery = request.query.data;
   const latitude = searchQuery.latitude;
   const longitude = searchQuery.longitude;
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${latitude},${longitude}`
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${latitude},${longitude}`;
 
   superagent
     .get(url)
-    .then(results => {
-      const weatherData = results.body.daily.data;
-      const weatherForecast = weatherData.map(dayObj => {
-        return new Weather(dayObj);
-      })
+    .then(result => {
+      const weatherData = result.body.daily.data;
+      const weatherForecast = weatherData.map(dayObj => new Weather(dayObj));
       response.status(200).send(weatherForecast);
     })
-    .catch(error => {
-      handleError(error, response);
-    })
+    .catch(error => handleError(error, response));
 }
 
-function Weather(dayObj){
+function Weather(dayObj) {
   this.forecast = dayObj.summary;
   this.time = this.formattedDate(dayObj.time);
 }
@@ -117,30 +100,33 @@ Weather.prototype.formattedDate = function(time) {
   return date.toDateString();
 }
 
+
+// A function to retreive event information for the searched location and render it on the page
 function getEvents(request, response) {
   const searchQuery = request.query.data;
   const latitude = searchQuery.latitude;
   const longitude = searchQuery.longitude;
   const url = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${longitude}&location.latitude=${latitude}74&expand=venue&token=${process.env.EVENTBRITE_PUBLIC_TOKEN}`;
 
-  superagent.get(url)
+  superagent
+    .get(url)
     .then(data => {
-      const events = data.body.events.map(obj => new Event(obj));
+      const events = data.body.events.map(eventObj => new Event(eventObj));
       response.status(200).send(events);
     })
-    .catch(err => handleError(err, response));
+    .catch(error => handleError(error, response));
 }
 
-function Event(obj) {
-  this.link = obj.url;
-  this.name = obj.name.text;
-  this.event_date = obj.start.local;
-  this.summary = obj.summary;
+function Event(eventObj) {
+  this.link = eventObj.url;
+  this.name = eventObj.name.text;
+  this.event_date = eventObj.start.local;
+  this.summary = eventObj.summary;
 }
 
+// A function to retreive movie information for the searched location and render it on the page
 function getMovies(request, response) {
   const searchQuery = request.query.data;
-  console.log(searchQuery.search_query);
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&query=${searchQuery.search_query}`;
   superagent
     .get(url)
@@ -150,7 +136,7 @@ function getMovies(request, response) {
       const topMovies = sortedMovies.splice(0, 20);
       response.status(200).send(topMovies);
     })
-    .catch(err => handleError(err, response));
+    .catch(error => handleError(error, response));
 }
 
 function Movie(movieObj) {
@@ -163,7 +149,9 @@ function Movie(movieObj) {
   this.released_on = movieObj.release_date;
 }
 
-function getYelp(request, response){
+
+// A function to retreive yelp information for the searched location and render it on the page
+function getYelp(request, response) {
   const searchQuery = request.query.data;
   const apiKey = process.env.YELP_API_KEY;
 
@@ -176,14 +164,10 @@ function getYelp(request, response){
   client
     .search(searchRequest)
     .then(data => {
-      // const restraunts = response.jsonBody.businesses;
-      // console.log(restraunts);})
       const restaurants = data.jsonBody.businesses.map(restaurantObj => new Restaurant(restaurantObj));
       response.status(200).send(restaurants);
     })
-    .catch(e => {
-      console.log(e);
-    });
+    .catch(error => handleError(error, response));
 }
 
 function Restaurant(restaurantObj) {
@@ -194,28 +178,55 @@ function Restaurant(restaurantObj) {
   this.url = restaurantObj.url;
 }
 
+// A function to retreive trail information for the searched location and render it on the page
+function getTrails(request, response) {
+  const searchQuery = request.query.data;
+  const latitude = searchQuery.latitude;
+  const longitude = searchQuery.longitude;
+  console.log(latitude);
+  console.log(longitude);
+  const url = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&key=${process.env.TRAIL_API_KEY}`;
 
+  superagent
+    .get(url)
+    .then(data => {
+      const trailData = data.body.trails;
+      const trailInformation = trailData.map(trailObj => new Trail(trailObj));
+      response.status(200).send(trailInformation);
+    })
+    .catch(error => handleError(error, response));
+}
 
+function Trail(trailObj) {
+  this.name = trailObj.name;
+  this.location = trailObj.location;
+  this.length = trailObj.length;
+  this.stars = trailObj.stars;
+  this.star_votes = trailObj.starVotes;
+  this.summary = trailObj.summary;
+  this.trail_url = trailObj.url;
+  this.conditions = trailObj.conditionStatus;
+  this.condition_date = trailObj.conditionDate.split(' ')[0];
+  this.condition_time = trailObj.conditionDate.split(' ')[1];
+}
 
+// A function to handle Error 404
 function wildcardRouter(request, response) {
-  response.status(500).send('Sorry, something went wrong');
+  response.status(404).send('Sorry, something went wrong');
 }
 
-/**
- * Helper Objects and Functions
- */
 
-function Error(err) {
-  this.status = 500;
-  this.responseText = 'Sorry, something went wrong. ' + JSON.stringify(err);
-  this.error = err;
-}
-
-function handleError(err, response) {
-  console.error(err);
-  const error = new Error(err);
+// A function to handle Error 500
+function handleError(error, response) {
+  console.error(error);
+  const err = new Error(error);
   if (response) {
-    response.status(error.status).send(error.responseText);
+    response.status(err.status).send(err.responseText);
   }
 }
 
+function Error(error) {
+  this.status = 500;
+  this.responseText = 'Sorry, something went wrong. ' + JSON.stringify(error);
+  this.error = error;
+}
